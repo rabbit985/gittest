7ef6f103557b87473b602e04df23f382397a04e0
LZ <79854420@qq.com>
2021-05-27 19:08:55 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @author 14761
+ * @create 2021/5/27 19:03
+ *
+ * 被忽略的文件没有被追踪，依旧是灰色的。
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
