7199d3b298a8ee0a718a282a07cfb421f38facc1
LZ <79854420@qq.com>
2021-05-27 19:14:09 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,9 +5,12 @@ package com.atguigu.git;
  * @create 2021/5/27 19:03
  *
  * 被忽略的文件没有被追踪，依旧是灰色的。
+ *  add 之后变为绿色，
+ *  commit 之后恢复原样，并且以后不用再添加
  */
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
